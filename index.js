64231e9d3d3f33771c5902fd996ef630ca82beed
puchauhan2 <puchauhan4@gmail.com>
2021-10-06 11:31:48 +0530

    added to get ip info

--- index.js
+++ index.js
@@ -4,21 +4,34 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
+
+var address = require('address');
+
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 if(os.hostname()) {
+    var ip = address.ip();
 	var hostname = os.hostname();
 	console.log("Hostname for the operating"
 		+ " system is " + String(hostname));
 }
 
 app.get('/hi', (req, res) => {
-    
+    var ip = address.ip();
     var hostname = os.hostname();
-    res.send(hostname);
+    res.send(hostname + " Ip is " + ip);
     });
+
+var ip = address.ip();   // '192.168.0.2'
+address.ipv6(); // 'fe80::7aca:39ff:feb0:e67d'
+address.mac(function (err, addr) {
+console.log(addr); // '78:ca:39:b0:e6:7d'
+console.log(ip)
+    });
+     
+
 
     app.listen(port, () => {
         console.log('Server started on port '+port);
